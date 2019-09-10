da0d3c4f47330edb0bafd2647c9a75ca9d8984c1
wml129 <1912152104@qq.com>
2019-09-10 20:13:17 +0800

    测试ssh2

--- src/jinqu/Test01.java
+++ src/jinqu/Test01.java
@@ -2,7 +2,7 @@ package jinqu;
 
 public class Test01 {
 	public static void main(String[] args) {
-		System.out.println("111111111112222wwww");
+		System.out.println("1111111111wwww");
 		System.out.println("2222222222222eeeeee");
 	}
 }

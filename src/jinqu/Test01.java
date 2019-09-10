8a87aedb1f8a43343eb5bdc57728d73bb703f600
wml129 <1912152104@qq.com>
2019-09-10 21:20:02 +0800

    更新测试4

--- src/jinqu/Test01.java
+++ src/jinqu/Test01.java
@@ -4,5 +4,6 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("1111111111wwww");
 		System.out.println("2222222222222eeeeee");
+		System.out.println("33333333333333");
 	}
 }
